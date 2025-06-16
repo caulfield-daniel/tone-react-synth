956ca8f987eba15f921e2e3dc287c35ec230ee9c
Daniel Caulfield <danil.tsapaev@bk.ru>
2025-06-16 13:35:25 +0300

    feat: package.json updates for gh-pages

--- frontend/src/components/Synth/Keyboard/Keyboard.style.js
+++ frontend/src/components/Synth/Keyboard/Keyboard.style.js
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 export const KeyboardContainer = styled.div`
     background-color:rgb(97, 97, 97);
     height: 200px;
-    width: 100%;
     display: flex;
     gap: 1px;
+    justify-content: center;
     flex-direction: row;
     position: relative;
     user-select: none;
